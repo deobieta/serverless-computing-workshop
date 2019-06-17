d19f05d92de9a5142da02e35aef4d3ba043e09c1
gabo de obieta <deobieta@gmail.com>
2019-06-17 18:10:40 -0500

    4-workshop-lambda-blackbox

---
+++ 4-workshop-lambda-blackbox/blackbox.tf
@@ -0,0 +1,11 @@
+
+module "lambda-api-gateway" {
+  source      = "../modules/lambda-api-gateway/"
+  name        = "simple-blackbox"
+  runtime     = "python3.6"
+  source_file = "function/main.py"
+}
+
+output "api_gateway_invoke_url" {
+  value = module.lambda-api-gateway.invoke_url
+}
--- 4-workshop-lambda-blackbox/function/main.py
+++ 4-workshop-lambda-blackbox/function/main.py
@@ -1,5 +1,5 @@
 def handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Hola desde Lambda con API gateway Version 1!'
+        'body': 'Hola desde Lambda con blackbox!'
     }
